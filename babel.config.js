53f791e59afca1ba24b8896bf1f5f292199823db
Yifei He <yifei@hesyifei.com>
2019-08-14 16:59:49 -0700

    Use `@emotion/babel-preset-css-prop` instead of `babel-plugin-emotion`

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["emotion"]
+    presets: ["babel-preset-expo", "@emotion/babel-preset-css-prop"]
   };
 };
