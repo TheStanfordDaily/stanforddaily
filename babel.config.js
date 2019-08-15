2cff8e67e1e02ca6914f96d6ceb74a07bb686c8f
Yifei He <yifei@hesyifei.com>
2019-08-15 15:49:19 -0700

    Revert "Use `@emotion/babel-preset-css-prop` instead of `babel-plugin-emotion`"
    This reverts commit 53f791e59afca1ba24b8896bf1f5f292199823db.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "@emotion/babel-preset-css-prop"]
+    presets: ["babel-preset-expo"],
+    plugins: ["emotion"]
   };
 };
