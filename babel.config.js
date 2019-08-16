b779b681db77dad5f23c58caf628981dc9230710
Yifei He <yifei@hesyifei.com>
2019-08-16 15:49:31 -0700

    Use `transform-remove-console` if not in dev mode

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
+/* eslint func-names: ["off"] */
 module.exports = function(api) {
   api.cache(true);
+
+  const isDev = process.env.NODE_ENV === "development";
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["emotion"]
+    plugins: ["emotion", !isDev && "transform-remove-console"].filter(Boolean),
   };
 };
