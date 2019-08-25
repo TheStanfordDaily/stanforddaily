a30822ba19c93ce648cfa631e670b620b9d8d470
Yifei He <yifei@hesyifei.com>
2019-08-25 15:06:55 -0700

    [babel] Use `@emotion/babel-preset-css-prop` preset for web

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,19 @@
-/* eslint func-names: ["off"] */
+/* eslint-disable */
 module.exports = function(api) {
-  api.cache(true);
-
+  const isWeb = api.caller(isTargetWeb);
   const isDev = process.env.NODE_ENV === "development";
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["emotion", !isDev && "transform-remove-console"].filter(Boolean),
+    presets: [
+      ["babel-preset-expo"],
+      isWeb && ["@emotion/babel-preset-css-prop"],
+    ].filter(Boolean),
+    plugins: [
+      !isWeb && ["emotion"],
+      !isDev && ["transform-remove-console"],
+    ].filter(Boolean),
   };
 };
+
+function isTargetWeb(caller) {
+  return caller && caller.name === "babel-loader";
+}
