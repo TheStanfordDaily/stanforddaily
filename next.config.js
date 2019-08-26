75a0fe112facc21367d7d3ce91f96223997aa040
Yifei He <yifei@hesyifei.com>
2019-08-26 09:07:51 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const withTM = require('next-transpile-modules');
+/* eslint-disable */
+const withTM = require("next-transpile-modules");
 
 module.exports = withTM({
   // TODO: I'm not sure exactly why we need this.
-  transpileModules: ["^@emotion"]
+  transpileModules: ["^@emotion"],
 });
