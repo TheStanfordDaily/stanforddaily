280b5a65287008dbac4812e35b795aef8706b457
Yifei He <yifei@hesyifei.com>
2019-08-26 17:13:40 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,5 @@ const withTM = require("next-transpile-modules");
 module.exports = withTM({
   // TODO: I'm not sure exactly why we need this.
   transpileModules: ["^@emotion"],
+  target: "serverless",
 });
