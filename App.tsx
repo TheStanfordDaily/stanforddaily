c927efb3a38fcc0656864b7e1d90484efcaaa148
Yifei He <yifei@hesyifei.com>
2019-09-04 14:55:07 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { IndexPageWrapper } from "./pages/index";
 import { PostPageWrapper } from "./pages/[year]/[month]/[day]/[slug]";
+import { CategoryArchivePageWrapper } from "./components/ArchivePage/CategoryArchivePage";
+import { TimeArchivePageWrapper } from "./components/ArchivePage/TimeArchivePage";
 
 const AppNavigator = createStackNavigator(
   {
     index: IndexPageWrapper,
     post: PostPageWrapper,
+    timeArchive: TimeArchivePageWrapper,
+    categoryArchive: CategoryArchivePageWrapper,
   },
   {
     initialRouteName: "index",
