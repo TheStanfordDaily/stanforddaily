d2b3618e047448883d1ba29b224ba337f7f0d8e0
Yifei He <yifei@hesyifei.com>
2019-09-26 17:49:06 -0700

    Add basic support for `p` (not redirecting to the correct post yet)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,57 @@
 import React from "react";
+import Router from "next/router";
 import HomePage from "components/pages/HomePage";
 import SearchArchivePage from "components/pages/SearchArchivePage";
 
+enum PageType {
+  SEARCH,
+  HOME,
+  REDIRECT_TO_POST,
+}
+
 export default class IndexPage extends React.Component<any, any> {
   static async getInitialProps(param): Promise<any> {
-    const { query } = param;
-    const { s = null }: { s?: string } = query;
+    const { query, res } = param;
+    const {
+      s = null,
+      p = null,
+    }: {
+      s?: string;
+      p?: string;
+    } = query;
     if (s) {
       const searchKeyword = s;
       const initProps = await SearchArchivePage.getSearchResults(searchKeyword);
-      return { ...initProps, isSearch: true };
+      return { ...initProps, pageType: PageType.SEARCH };
+    } else if (p) {
+      // https://github.com/zeit/next.js/wiki/Redirecting-in-%60getInitialProps%60
+      const redirectLocation = "/about"; // TODO
+      if (res) {
+        res.writeHead(301, {
+          Location: redirectLocation,
+        });
+        res.end();
+      } else {
+        Router.push(redirectLocation);
+      }
+      return { pageType: PageType.REDIRECT_TO_POST };
     } else {
       const initProps = await HomePage.getInitialProps();
-      return { ...initProps, isSearch: false };
+      return { ...initProps, pageType: PageType.HOME };
     }
-    // TODO: CONSIDER `?p=`.
   }
 
   render(): React.ReactNode {
-    const { isSearch, ...restProps } = this.props;
-    if (isSearch) {
-      return <SearchArchivePage {...(restProps as any)} />;
-    } else {
-      return <HomePage {...(restProps as any)} />;
+    const { pageType, ...restProps } = this.props;
+    switch (pageType) {
+      case PageType.HOME:
+        return <HomePage {...(restProps as any)} />;
+
+      case PageType.SEARCH:
+        return <SearchArchivePage {...(restProps as any)} />;
+
+      default:
+        return <></>;
     }
   }
 }
