dd2e240aef719ccb774a70b4ea665458c4b12d73
Holden Foreman <hs4man21@stanford.edu>
2019-10-16 23:34:58 -0700

    Update SportsSection.tsx

--- components/pages/HomePage/SportsSection.tsx
+++ components/pages/HomePage/SportsSection.tsx
@@ -54,6 +54,7 @@ export const SportsSection: React.ElementType = (props: SectionProps) => {
               accessibilityLabel="Sports"
               resizeMode="contain"
               style={{
+                alignContent: "flex-start",
                 height: 30,
               }}
             />
