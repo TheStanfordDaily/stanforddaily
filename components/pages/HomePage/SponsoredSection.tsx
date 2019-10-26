68092715506157589d1613a53ce54bc96af3a707
Holden Foreman <hs4man21@stanford.edu>
2019-10-26 11:04:12 -0700

    Update SponsoredSection.tsx

--- components/pages/HomePage/SponsoredSection.tsx
+++ components/pages/HomePage/SponsoredSection.tsx
@@ -5,37 +5,37 @@ import { RightListedSection } from "./RightListedSection";
 import { SectionTitle } from "./SectionTitle";
 import { SectionProps } from "./SectionProps";
 
-export const SponsoredSection: React.ElementType = ({
-  content,
-  ...props
-}: SectionProps) => {
-  return (
-    <RightListedSection
-      displayAuthor={false}
-      content={content}
-      sectionTitle="Sponsored Content"
-      {...props}
-    />
-  );
-};
-
-export const MoreFromTheDailySection: React.ElementType = ({
-  content,
-}: SectionProps) => {
-  return (
-    <Section
-      style={{
-        flexGrow: 1,
-        flexDirection: "column",
-      }}
-    >
-      <SectionTitle>More from The Daily</SectionTitle>
-      <ArticlesView
-        initPosts={content}
-        // getExtraPosts={async pageNumber => {
-        //   return getHomeMoreAsync(pageNumber);
-        // }}
-      />
-    </Section>
-  );
-};
+// export const SponsoredSection: React.ElementType = ({
+//   content,
+//   ...props
+// }: SectionProps) => {
+//   return (
+//     <RightListedSection
+//       displayAuthor={false}
+//       content={content}
+//       sectionTitle="Sponsored Content"
+//       {...props}
+//     />
+//   );
+// };
+//
+// export const MoreFromTheDailySection: React.ElementType = ({
+//   content,
+// }: SectionProps) => {
+//   return (
+//     <Section
+//       style={{
+//         flexGrow: 1,
+//         flexDirection: "column",
+//       }}
+//     >
+//       <SectionTitle>More from The Daily</SectionTitle>
+//       <ArticlesView
+//         initPosts={content}
+//         // getExtraPosts={async pageNumber => {
+//         //   return getHomeMoreAsync(pageNumber);
+//         // }}
+//       />
+//     </Section>
+//   );
+// };
