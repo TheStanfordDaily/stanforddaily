1d0f32ef73d2f762fd97646e190cb3a48a6a7e84
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-11-18 16:50:12 +0000

    add ezoic ad placeholder

--- components/pages/HomePage/index.tsx
+++ components/pages/HomePage/index.tsx
@@ -156,6 +156,7 @@ export default class HomePage extends React.Component<IndexProps, IndexState> {
                 },
               }}
             >
+              <div id="ezoic-pub-ad-placeholder-101"></div>
               <OpinionSection
                 content={homePosts.opinions}
                 category={homePosts.tsdMeta.categories.opinions}
