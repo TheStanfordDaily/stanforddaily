958158138574f65487ae71f41e4d05b883dc9cb0
hs4man21 <hs4man21@stanford.edu>
2019-12-10 12:25:28 -0800

    Update ArticlesView.tsx

--- components/ArticlesView.tsx
+++ components/ArticlesView.tsx
@@ -54,7 +54,6 @@ interface ArticlesViewProps {
   displayExcerpt?: boolean;
   displayDateAuthor?: boolean;
   textColor?: string;
-  noCartoons?: boolean;
 }
 
 const ArticlesView: React.ElementType<ArticlesViewProps> = ({
@@ -65,7 +64,6 @@ const ArticlesView: React.ElementType<ArticlesViewProps> = ({
   displayExcerpt = true,
   displayDateAuthor = true,
   textColor = STANFORD_COLORS.BLACK,
-  noCartoons = true,
 }: ArticlesViewProps) => {
   const [extraPosts, setExtraPosts]: [Post[], any] = React.useState([]);
   const [extraPageNumber, setExtraPageNumber]: [number, any] = React.useState(
@@ -91,21 +89,17 @@ const ArticlesView: React.ElementType<ArticlesViewProps> = ({
         marginRight: -SECTION_PADDING,
       }}
     >
-      {initPosts.concat(extraPosts).map(
-        post =>
-          ((!noCartoons && post.tsdPrimaryCategory.name !== "Cartoons") ||
-            noCartoons) && (
-            <EachArticleView key={post.id} displayExcerpt={displayExcerpt}>
-              <TextOnlyArticle
-                post={post}
-                displayCategory={displayCategory}
-                displayExcerpt={displayExcerpt}
-                displayDateAuthor={displayDateAuthor}
-                textColor={textColor}
-              />
-            </EachArticleView>
-          ),
-      )}
+      {initPosts.concat(extraPosts).map(post => (
+        <EachArticleView key={post.id} displayExcerpt={displayExcerpt}>
+          <TextOnlyArticle
+            post={post}
+            displayCategory={displayCategory}
+            displayExcerpt={displayExcerpt}
+            displayDateAuthor={displayDateAuthor}
+            textColor={textColor}
+          />
+        </EachArticleView>
+      ))}
       {[...Array(6)].map((value, index) => (
         // Make sure the last row of articles will not stretch.
         // https://jsfiddle.net/7yr86aow/3/
