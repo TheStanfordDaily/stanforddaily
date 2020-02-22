dd1298137e73a24808aec85758834e8375052afa
hs4man21 <38192823+hs4man21@users.noreply.github.com>
2020-02-22 13:06:54 -0800

    Small wording changes on top bar (#69)
    * Update TopSection.tsx
    
    * Update TopSection.tsx

--- components/pages/HomePage/TopSection.tsx
+++ components/pages/HomePage/TopSection.tsx
@@ -164,14 +164,14 @@ export const TopSection: React.ElementType = ({ style }) => {
               url={LINKS.ISSUU}
               imageUrl={LINKS.ISSUU_LOGO}
               header="Newspaper & Magazine"
-              title="Read the Print Issues"
+              title="Read Our Print Issues"
               newTab
             />
             <SmallSection
               url="/category/newsletters/"
               imageUrl={LINKS.NEWSLETTER_LOGO}
               header="Weekend Roundup"
-              title="Read our Weekly Newsletter"
+              title="Read Our Weekly Newsletter"
               newTab
             />
             {/* <SmallSection
@@ -205,7 +205,7 @@ export const TopSection: React.ElementType = ({ style }) => {
                 marginRight: 15,
               }}
             />
-            <TextButtonWithLink url="/email-digests/" title="Email Digest" />
+            <TextButtonWithLink url="/email-digests/" title="Get Our Emails" />
           </ViewRow>
         </View>
       </SectionStyle>
