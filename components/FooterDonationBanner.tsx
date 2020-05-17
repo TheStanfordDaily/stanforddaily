3836466410868cecbad5fb14f9b2439c95664d46
amelia22974 <35205094+amelia22974@users.noreply.github.com>
2020-05-17 12:13:43 -0600

    Added social media and email sign up links to donation footer (#111)
    Co-authored-by: amelia22974 <amelia.a.woodward@gmail.cpm>

--- components/FooterDonationBanner.tsx
+++ components/FooterDonationBanner.tsx
@@ -1,6 +1,58 @@
 import React from "react";
 import { SECTION_PADDING } from "components/Section";
 import DonationForm from "./DonationForm";
+import LogoFacebook from "react-ionicons/lib/LogoFacebook";
+import LogoTwitter from "react-ionicons/lib/LogoTwitter";
+import LogoInstagram from "react-ionicons/lib/LogoInstagram";
+import LogoYoutube from "react-ionicons/lib/LogoYoutube";
+import { LINKS, FONTS, STANFORD_COLORS } from "helpers/constants";
+import Link from "components/Link";
+
+const LogoIconWithLink: React.ElementType = ({ url, LogoComponent }: any) => (
+  <a
+    href={url}
+    target="_blank"
+    rel="noopener noreferrer"
+    style={{
+      display: "inline-flex",
+      marginRight: 20,
+    }}
+  >
+    <LogoComponent className="headerLogoIcon" fontSize="25px" />
+  </a>
+);
+
+const TextButtonWithLink: React.ElementType = ({
+  url,
+  urlFile = "/[year]/",
+  title,
+  mobileTitle,
+  tbwlStyle,
+}: any) => (
+  <Link href={urlFile} as={url}>
+    <a
+      title={title}
+      style={{
+        ...FONTS.AUXILIARY,
+        color: STANFORD_COLORS.CARDINAL_RED,
+        border: `2px ${STANFORD_COLORS.CARDINAL_RED} solid`,
+        fontSize: 14,
+        letterSpacing: 1.5,
+        padding: 8,
+        textAlign: "center",
+        ...tbwlStyle,
+      }}
+    >
+      {!mobileTitle && title}
+      {mobileTitle && (
+        <>
+          <span className="hidden-mobile">{title}</span>
+          <span className="visible-mobile">{mobileTitle}</span>
+        </>
+      )}
+    </a>
+  </Link>
+);
 
 const FooterDonationBanner: React.ElementType = ({ currentPageUrl }) => {
   return (
@@ -32,7 +84,21 @@ const FooterDonationBanner: React.ElementType = ({ currentPageUrl }) => {
         professional skills and conduct meaningful reporting. All contributions
         are tax-deductible.
       </p>
+
       <DonationForm currentPageUrl={currentPageUrl} bannerLocation="Footer" />
+      <br></br>
+      <TextButtonWithLink
+        url="/email-digests/"
+        title="Get Our Emails"
+        mobileTitle="Digest"
+      />
+      <div>
+        <br></br>
+        <LogoIconWithLink url={LINKS.FACEBOOK} LogoComponent={LogoFacebook} />
+        <LogoIconWithLink url={LINKS.TWITTER} LogoComponent={LogoTwitter} />
+        <LogoIconWithLink url={LINKS.INSTAGRAM} LogoComponent={LogoInstagram} />
+        <LogoIconWithLink url={LINKS.YOUTUBE} LogoComponent={LogoYoutube} />
+      </div>
     </div>
   );
 };
