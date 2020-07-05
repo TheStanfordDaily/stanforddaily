ede77919ac455620dd07bd2b910df093f0cb2fb8
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-07-05 15:24:58 -0400

    Don't redirect errors to /wp-json/

---
+++ pages/wp-json/[...slug].tsx
@@ -0,0 +1,9 @@
+import Redirect from "../_redirect";
+
+/**
+ * This page is called [...slug].tsx, which allows it to match arbitrarily nested paths.
+ * This means that all paths under https://stanforddaily.com/wp-json/... will be redirected
+ * to their corresponding https://wp.stanforddaily.com/wp-json/... paths.
+ */
+
+export default Redirect;
