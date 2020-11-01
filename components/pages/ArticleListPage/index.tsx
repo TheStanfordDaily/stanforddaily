6db0e6b9f15644dbe1b56371833fdbbe014fee27
hs4man21 <hs4man21@stanford.edu>
2020-11-01 12:05:55 -0800

    Used square bracket indexing instead of Object.property approach to access post.postCategory and initData.tsdMeta.title

--- components/pages/ArticleListPage/index.tsx
+++ components/pages/ArticleListPage/index.tsx
@@ -55,16 +55,16 @@ export default class ArticleListPage extends React.Component<
 
     const articlesView = (
       <>
-        {typeof window !== "undefined" &&
-          window.location.href.includes("/category/us-elections-2020/") && (
-            <>
-              <DesktopRow
-                style={{
-                  ...getBorderValue("Bottom"),
-                }}
-              >
-                {console.log(initData)}
-                {/* <Column
+        {initData.tsdMeta["title"] === "US Elections 2020" && (
+          <>
+            {console.log(initData)}
+            <DesktopRow
+              style={{
+                ...getBorderValue("Bottom"),
+              }}
+            >
+              {console.log(initData)}
+              {/* <Column
         style={{
           flex: 1
         }}
@@ -79,116 +79,116 @@ export default class ArticleListPage extends React.Component<
         {/* <TopThumbnailArticle post={initData.posts[1]} large={true} /> 
         <HeadlineArticle post={initData.posts[1]} style={{ marginBottom: 20 }} />
       </Column> */}
-                <Column
-                  style={{
-                    flex: 1,
-                  }}
-                  rStyle={{
-                    [MediaRule.MinWidth]: {
-                      [BREAKPOINTS.TABLET]: {
-                        // paddingLeft: SECTION_PADDING / 2,
-                        paddingRight: SECTION_PADDING / 2,
-                        //       ...getBorderValue("Left"),
-                      },
+              <Column
+                style={{
+                  flex: 1,
+                }}
+                rStyle={{
+                  [MediaRule.MinWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      // paddingLeft: SECTION_PADDING / 2,
+                      paddingRight: SECTION_PADDING / 2,
+                      //       ...getBorderValue("Left"),
                     },
+                  },
+                }}
+              >
+                <TopThumbnailArticle
+                  post={
+                    initData.posts.filter(post =>
+                      post["postCategory"].includes(70941),
+                    )[0]
+                  }
+                />
+                <TopThumbnailArticle
+                  post={
+                    initData.posts.filter(post =>
+                      post["postCategory"].includes(70941),
+                    )[1]
+                  }
+                />
+              </Column>
+              <Column
+                style={{
+                  flex: 3,
+                }}
+                rStyle={{
+                  [MediaRule.MinWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      paddingLeft: SECTION_PADDING / 2,
+                      paddingRight: SECTION_PADDING / 2,
+                      paddingBottom: SECTION_PADDING / 2,
+                      ...getBorderValue("Left"),
+                    },
+                  },
+                  [MediaRule.MaxWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      height: 300,
+                    },
+                  },
+                }}
+              >
+                <h1 style={{ marginTop: "0px", marginBottom: "5px" }}>
+                  Tracking the Stanford alums running for office
+                </h1>
+                <Text
+                  style={{
+                    marginBottom: "5px",
+                    ...FONTS.CONTENT,
+                    ...(Platform.OS === "web" ? { lineHeight: "1.2em" } : {}),
                   }}
                 >
-                  <TopThumbnailArticle
-                    post={
-                      initData.posts.filter(post =>
-                        post.postCategory.includes(70941),
-                      )[0]
-                    }
-                  />
-                  <TopThumbnailArticle
-                    post={
-                      initData.posts.filter(post =>
-                        post.postCategory.includes(70941),
-                      )[1]
-                    }
-                  />
-                </Column>
-                <Column
-                  style={{
-                    flex: 3,
-                  }}
-                  rStyle={{
-                    [MediaRule.MinWidth]: {
-                      [BREAKPOINTS.TABLET]: {
-                        paddingLeft: SECTION_PADDING / 2,
-                        paddingRight: SECTION_PADDING / 2,
-                        paddingBottom: SECTION_PADDING / 2,
-                        ...getBorderValue("Left"),
-                      },
-                    },
-                    [MediaRule.MaxWidth]: {
-                      [BREAKPOINTS.TABLET]: {
-                        height: 300,
-                      },
-                    },
-                  }}
-                >
-                  <h1 style={{ marginTop: "0px", marginBottom: "5px" }}>
-                    Tracking the Stanford alums running for office
-                  </h1>
-                  <Text
-                    style={{
-                      marginBottom: "5px",
-                      ...FONTS.CONTENT,
-                      ...(Platform.OS === "web" ? { lineHeight: "1.2em" } : {}),
-                    }}
-                  >
-                    Are we missing anything? <a href="/tips/">Send us a tip</a>{" "}
-                    so we can update the map!
-                  </Text>
-                  <UsElections2020Map />
-                  {console.log(initData.tsdMeta)}
-                  {/* <TopThumbnailArticle post={initData.posts[2]} />
+                  Are we missing anything? <a href="/tips/">Send us a tip</a> so
+                  we can update the map!
+                </Text>
+                <UsElections2020Map />
+                {console.log(initData.tsdMeta)}
+                {/* <TopThumbnailArticle post={initData.posts[2]} />
                 <TopThumbnailArticle post={initData.posts[2]} /> */}
-                </Column>
-                <Column
-                  style={{
-                    flex: 2,
-                  }}
-                  rStyle={{
-                    [MediaRule.MinWidth]: {
-                      [BREAKPOINTS.TABLET]: {
-                        paddingLeft: SECTION_PADDING / 2,
-                        paddingRight: SECTION_PADDING / 2,
-                        ...getBorderValue("Left"),
-                      },
+              </Column>
+              <Column
+                style={{
+                  flex: 2,
+                }}
+                rStyle={{
+                  [MediaRule.MinWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      paddingLeft: SECTION_PADDING / 2,
+                      paddingRight: SECTION_PADDING / 2,
+                      ...getBorderValue("Left"),
                     },
-                    [MediaRule.MaxWidth]: {
-                      [BREAKPOINTS.TABLET]: {
-                        height: 300,
-                      },
+                  },
+                  [MediaRule.MaxWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      height: 300,
                     },
-                  }}
-                >
-                  <div className="centerContent" style={{ height: "100%" }}>
-                    <div
-                      className="selfCenter standardWidth"
-                      style={{ height: "100%" }}
-                    >
-                      <TwitterTimelineEmbed
-                        sourceType="list"
-                        id={"1322794375890436099"}
-                        slug={null}
-                        autoHeight={true}
-                        //options={{ height: 400 }}
-                      />
-                    </div>
+                  },
+                }}
+              >
+                <div className="centerContent" style={{ height: "100%" }}>
+                  <div
+                    className="selfCenter standardWidth"
+                    style={{ height: "100%" }}
+                  >
+                    <TwitterTimelineEmbed
+                      sourceType="list"
+                      id={"1322794375890436099"}
+                      slug={null}
+                      autoHeight={true}
+                      //options={{ height: 400 }}
+                    />
                   </div>
-                </Column>
-              </DesktopRow>
-              <h1>More elections coverage</h1>
-            </>
-          )}
+                </div>
+              </Column>
+            </DesktopRow>
+            <h1>More elections coverage</h1>
+          </>
+        )}
         <ArticlesView
           displayCategory={displayCategory}
           displayExcerpt={displayExcerpt}
           initPosts={initData.posts.filter(
-            post => !post.postCategory.includes(70941),
+            post => !post["postCategory"].includes(70941),
           )}
           getExtraPosts={async pageNumber => {
             const extraData = await getExtraData(pageNumber);
