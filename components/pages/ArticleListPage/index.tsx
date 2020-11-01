84fde8c58c783f5231f371d2bb5d80ae3e29ea83
hs4man21 <hs4man21@stanford.edu>
2020-11-01 12:00:27 -0800

    Use window to check that we are in correct category page; remove unneeded imports and adjust mobile height of map and Twitter feed

--- components/pages/ArticleListPage/index.tsx
+++ components/pages/ArticleListPage/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { Text, Platform } from "react-native";
-import {
-  MediaRule,
-  isWidthGreaterThanOrEqualTo,
-} from "emotion-native-media-query";
+import { MediaRule } from "emotion-native-media-query";
 import css from "@emotion/css";
 import { FONTS, BREAKPOINTS } from "helpers/constants";
 import { ArticleListPageData } from "helpers/wpapi";
@@ -16,7 +13,6 @@ import { DesktopRow } from "../HomePage/DesktopRow";
 import { Column } from "../HomePage/Column";
 import { SECTION_PADDING } from "components/Section";
 import { getBorderValue } from "../HomePage/getBorderValue";
-import { HeadlineArticle } from "../HomePage/HeadlineArticle";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 import { UsElections2020Map } from "./UsElections2020Map";
 
@@ -59,15 +55,16 @@ export default class ArticleListPage extends React.Component<
 
     const articlesView = (
       <>
-        {initData.tsdMeta.title === "US Elections 2020" && (
-          <>
-            <DesktopRow
-              style={{
-                ...getBorderValue("Bottom"),
-              }}
-            >
-              {console.log(initData)}
-              {/* <Column
+        {typeof window !== "undefined" &&
+          window.location.href.includes("/category/us-elections-2020/") && (
+            <>
+              <DesktopRow
+                style={{
+                  ...getBorderValue("Bottom"),
+                }}
+              >
+                {console.log(initData)}
+                {/* <Column
         style={{
           flex: 1
         }}
@@ -82,105 +79,111 @@ export default class ArticleListPage extends React.Component<
         {/* <TopThumbnailArticle post={initData.posts[1]} large={true} /> 
         <HeadlineArticle post={initData.posts[1]} style={{ marginBottom: 20 }} />
       </Column> */}
-              <Column
-                style={{
-                  flex: 1,
-                }}
-                rStyle={{
-                  [MediaRule.MinWidth]: {
-                    [BREAKPOINTS.TABLET]: {
-                      // paddingLeft: SECTION_PADDING / 2,
-                      paddingRight: SECTION_PADDING / 2,
-                      //       ...getBorderValue("Left"),
-                    },
-                  },
-                }}
-              >
-                <TopThumbnailArticle
-                  post={
-                    initData.posts.filter(post =>
-                      post.postCategory.includes(70941),
-                    )[0]
-                  }
-                />
-                <TopThumbnailArticle
-                  post={
-                    initData.posts.filter(post =>
-                      post.postCategory.includes(70941),
-                    )[1]
-                  }
-                />
-              </Column>
-              <Column
-                style={{
-                  flex: 3,
-                }}
-                rStyle={{
-                  [MediaRule.MinWidth]: {
-                    [BREAKPOINTS.TABLET]: {
-                      paddingLeft: SECTION_PADDING / 2,
-                      paddingRight: SECTION_PADDING / 2,
-                      paddingBottom: SECTION_PADDING / 2,
-                      ...getBorderValue("Left"),
-                    },
-                  },
-                  [MediaRule.MaxWidth]: {
-                    [BREAKPOINTS.TABLET]: {
-                      height: 400,
-                    },
-                  },
-                }}
-              >
-                <h1 style={{ marginTop: "0px", marginBottom: "5px" }}>
-                  Tracking the Stanford alums running for office
-                </h1>
-                <Text
+                <Column
                   style={{
-                    marginBottom: "5px",
-                    ...FONTS.CONTENT,
-                    ...(Platform.OS === "web" ? { lineHeight: "1.2em" } : {}),
+                    flex: 1,
+                  }}
+                  rStyle={{
+                    [MediaRule.MinWidth]: {
+                      [BREAKPOINTS.TABLET]: {
+                        // paddingLeft: SECTION_PADDING / 2,
+                        paddingRight: SECTION_PADDING / 2,
+                        //       ...getBorderValue("Left"),
+                      },
+                    },
                   }}
                 >
-                  Are we missing anything? <a href="/tips/">Send us a tip</a> so
-                  we can update the map!
-                </Text>
-                <UsElections2020Map />
-                {/* <TopThumbnailArticle post={initData.posts[2]} />
-                <TopThumbnailArticle post={initData.posts[2]} /> */}
-              </Column>
-              <Column
-                style={{
-                  flex: 2,
-                }}
-                rStyle={{
-                  [MediaRule.MinWidth]: {
-                    [BREAKPOINTS.TABLET]: {
-                      paddingLeft: SECTION_PADDING / 2,
-                      paddingRight: SECTION_PADDING / 2,
-                      ...getBorderValue("Left"),
+                  <TopThumbnailArticle
+                    post={
+                      initData.posts.filter(post =>
+                        post.postCategory.includes(70941),
+                      )[0]
+                    }
+                  />
+                  <TopThumbnailArticle
+                    post={
+                      initData.posts.filter(post =>
+                        post.postCategory.includes(70941),
+                      )[1]
+                    }
+                  />
+                </Column>
+                <Column
+                  style={{
+                    flex: 3,
+                  }}
+                  rStyle={{
+                    [MediaRule.MinWidth]: {
+                      [BREAKPOINTS.TABLET]: {
+                        paddingLeft: SECTION_PADDING / 2,
+                        paddingRight: SECTION_PADDING / 2,
+                        paddingBottom: SECTION_PADDING / 2,
+                        ...getBorderValue("Left"),
+                      },
                     },
-                  },
-                }}
-              >
-                <div className="centerContent" style={{ height: "100%" }}>
-                  <div
-                    className="selfCenter standardWidth"
-                    style={{ height: "100%" }}
+                    [MediaRule.MaxWidth]: {
+                      [BREAKPOINTS.TABLET]: {
+                        height: 300,
+                      },
+                    },
+                  }}
+                >
+                  <h1 style={{ marginTop: "0px", marginBottom: "5px" }}>
+                    Tracking the Stanford alums running for office
+                  </h1>
+                  <Text
+                    style={{
+                      marginBottom: "5px",
+                      ...FONTS.CONTENT,
+                      ...(Platform.OS === "web" ? { lineHeight: "1.2em" } : {}),
+                    }}
                   >
-                    <TwitterTimelineEmbed
-                      sourceType="list"
-                      id={"1322794375890436099"}
-                      slug={null}
-                      autoHeight={true}
-                      //options={{ height: 400 }}
-                    />
+                    Are we missing anything? <a href="/tips/">Send us a tip</a>{" "}
+                    so we can update the map!
+                  </Text>
+                  <UsElections2020Map />
+                  {console.log(initData.tsdMeta)}
+                  {/* <TopThumbnailArticle post={initData.posts[2]} />
+                <TopThumbnailArticle post={initData.posts[2]} /> */}
+                </Column>
+                <Column
+                  style={{
+                    flex: 2,
+                  }}
+                  rStyle={{
+                    [MediaRule.MinWidth]: {
+                      [BREAKPOINTS.TABLET]: {
+                        paddingLeft: SECTION_PADDING / 2,
+                        paddingRight: SECTION_PADDING / 2,
+                        ...getBorderValue("Left"),
+                      },
+                    },
+                    [MediaRule.MaxWidth]: {
+                      [BREAKPOINTS.TABLET]: {
+                        height: 300,
+                      },
+                    },
+                  }}
+                >
+                  <div className="centerContent" style={{ height: "100%" }}>
+                    <div
+                      className="selfCenter standardWidth"
+                      style={{ height: "100%" }}
+                    >
+                      <TwitterTimelineEmbed
+                        sourceType="list"
+                        id={"1322794375890436099"}
+                        slug={null}
+                        autoHeight={true}
+                        //options={{ height: 400 }}
+                      />
+                    </div>
                   </div>
-                </div>
-              </Column>
-            </DesktopRow>
-            <h1>More elections coverage</h1>
-          </>
-        )}
+                </Column>
+              </DesktopRow>
+              <h1>More elections coverage</h1>
+            </>
+          )}
         <ArticlesView
           displayCategory={displayCategory}
           displayExcerpt={displayExcerpt}
