0b7452b3482731019c1bd74dd236cb57819274e2
hs4man21 <hs4man21@stanford.edu>
2020-11-09 23:57:02 -0800

    Style improvements to make spacing less off

--- components/ContentViewStyles.tsx
+++ components/ContentViewStyles.tsx
@@ -69,23 +69,22 @@ export default {
       },
     },
     blockquote: {
-      display: "block !important",
-      width: "350px !important",
+      display: "flex !important",
+      flexDirection: "column",
+      justifyContent: "center !important",
       padding: "5px 0 0 0px !important",
       borderLeft: "3px solid #820000 !important",
       cite: {
-        width: "350px !important",
         fontSize: "1.3em !important",
         textAlign: "center",
       },
       p: {
-        width: "350px !important",
         fontSize: "1.6em !important",
         lineHeight: "normal",
         textAlign: "left",
         overflowWrap: "break-word",
         padding: "25px 25px 0 25px !important",
-        backgroundImage: `url(${"../static/pullquote.gif"})`,
+        backgroundImage: `url(${"../assets/pullquote.gif"})`,
         backgroundSize: "20%",
         backgroundRepeat: "no-repeat !important",
         "&::first-letter": {
@@ -93,17 +92,17 @@ export default {
           fontWeight: "bold !important",
         },
       },
-      table: {
-        width: "100%",
-        borderCollapse: "collapse",
-        textAlign: "center",
-        border: `1px solid ${STANFORD_COLORS.COOL_GREY}`,
-        fontSize: "1.3rem",
-      },
-      td: {
-        border: `1px solid ${STANFORD_COLORS.COOL_GREY}`,
-        padding: "10px",
-      },
+    },
+    table: {
+      width: "100%",
+      borderCollapse: "collapse",
+      textAlign: "center",
+      border: `1px solid ${STANFORD_COLORS.COOL_GREY}`,
+      fontSize: "1.3rem",
+    },
+    td: {
+      border: `1px solid ${STANFORD_COLORS.COOL_GREY}`,
+      padding: "10px",
     },
   },
 };
