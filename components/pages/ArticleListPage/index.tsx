0a0d93a9964be0c3f68d77d769d58d6c97f1c4d5
hs4man21 <hs4man21@stanford.edu>
2020-11-15 05:57:06 -0800

    WIP draft of data section page top row

--- components/pages/ArticleListPage/index.tsx
+++ components/pages/ArticleListPage/index.tsx
@@ -14,6 +14,9 @@ import { SECTION_PADDING } from "components/Section";
 import { getBorderValue } from "../HomePage/getBorderValue";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 import { UsElections2020Map } from "./UsElections2020Map";
+import { CovidDataWidget } from "../HomePage/CovidDataWidget";
+import { HeadlineArticle } from "../HomePage/HeadlineArticle";
+import { TitleOnlyArticle } from "../HomePage/TitleOnlyArticle";
 
 export enum ArticleListPageType {
   Time, // TODO
@@ -54,9 +57,137 @@ export default class ArticleListPage extends React.Component<
 
     const articlesView = (
       <>
+        {initData.tsdMeta["title"] === "Data Team" && (
+          <>
+            <DesktopRow
+              style={{
+                ...getBorderValue("Bottom"),
+              }}
+            >
+              <Column
+                style={{
+                  flex: 3,
+                }}
+                rStyle={{
+                  [MediaRule.MinWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      // paddingLeft: SECTION_PADDING / 2,
+                      paddingRight: SECTION_PADDING / 2,
+                      //       ...getBorderValue("Left"),
+                    },
+                  },
+                }}
+              >
+                <HeadlineArticle
+                  post={initData.posts[0]}
+                  style={{ marginBottom: 20 }}
+                />
+              </Column>
+              <Column
+                style={{
+                  flex: 1,
+                }}
+                rStyle={{
+                  [MediaRule.MinWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      paddingLeft: SECTION_PADDING / 2,
+                      paddingRight: SECTION_PADDING / 2,
+                      paddingBottom: SECTION_PADDING / 2,
+                      ...getBorderValue("Left"),
+                    },
+                  },
+                  [MediaRule.MaxWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      height: 300,
+                    },
+                  },
+                }}
+              >
+                <TitleOnlyArticle
+                  post={initData.posts[0]}
+                  style={{ marginBottom: 20 }}
+                />
+                <TitleOnlyArticle
+                  post={initData.posts[0]}
+                  style={{ marginBottom: 20 }}
+                />
+                <TitleOnlyArticle
+                  post={initData.posts[0]}
+                  style={{ marginBottom: 20 }}
+                />
+              </Column>
+              <Column
+                style={{
+                  flex: 2,
+                }}
+                rStyle={{
+                  [MediaRule.MinWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      paddingLeft: SECTION_PADDING / 2,
+                      paddingRight: SECTION_PADDING / 2,
+                      ...getBorderValue("Left"),
+                    },
+                  },
+                  [MediaRule.MaxWidth]: {
+                    [BREAKPOINTS.TABLET]: {
+                      height: 300,
+                    },
+                  },
+                }}
+              >
+                <div className="centerContent" style={{ height: "100%" }}>
+                  <div
+                    className="selfCenter standardWidth"
+                    style={{ height: "100%" }}
+                  >
+                    <CovidDataWidget />
+                    <div>
+                      <div
+                        style={{
+                          fontFamily: "IBM Plex Sans Condensed, sans-serif",
+                          lineHeight: "1em",
+                          textTransform: "uppercase",
+                          marginTop: 20,
+                        }}
+                      >
+                        <a
+                          title="Stanford in the 2010s"
+                          style={{ color: "inherit" }}
+                          href="/category/@94305/"
+                        >
+                          <h1
+                            style={{
+                              fontFamily: "Open Sans, sans-serif",
+                              fontWeight: 900,
+                              fontSize: "20px",
+                              lineHeight: "normal",
+                              color: "#820000",
+                              margin: 0,
+                              textTransform: "none",
+                            }}
+                          >
+                            Stanford in the 2010s
+                          </h1>
+                        </a>
+                      </div>
+                    </div>
+                    <TitleOnlyArticle
+                      post={initData.posts[0]}
+                      style={{ marginBottom: 20 }}
+                    />
+                    <TitleOnlyArticle
+                      post={initData.posts[0]}
+                      style={{ marginBottom: 20 }}
+                    />
+                  </div>
+                </div>
+              </Column>
+            </DesktopRow>
+            <h1>More Data Team coverage</h1>
+          </>
+        )}
         {initData.tsdMeta["title"] === "US Elections 2020" && (
           <>
-            {console.log(initData)}
             <DesktopRow
               style={{
                 ...getBorderValue("Bottom"),
@@ -125,9 +256,6 @@ export default class ArticleListPage extends React.Component<
                   we can update the map!
                 </Text>
                 <UsElections2020Map />
-                {console.log(initData.tsdMeta)}
-                {/* <TopThumbnailArticle post={initData.posts[2]} />
-                <TopThumbnailArticle post={initData.posts[2]} /> */}
               </Column>
               <Column
                 style={{
