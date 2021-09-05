579ee54126da4b416f78512fb056d4fc2a1d7b39
hs4man21 <hs4man21@stanford.edu>
2021-09-04 17:53:32 -0700

    Try parse.ly script in WPFooter

--- components/webHelpers/WPFooter.tsx
+++ components/webHelpers/WPFooter.tsx
@@ -3,7 +3,13 @@ import { BaseProps } from "./baseTypes";
 
 const WPFooter: React.ElementType<BaseProps> = () => {
   // We don't need `wp_footer()` in the native app.
-  return <></>;
+  return (
+    /* Parse.ly analytics tracking */
+    <script
+      id="parsely-cfg"
+      src="//cdn.parsely.com/keys/sandbox.stanforddaily.com/p.js"
+    />
+  );
 };
 
 export default WPFooter;
